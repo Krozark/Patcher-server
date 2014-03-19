06e25343e56ad1bd0977dac9b6e6c83249cb692a
Krozark <maxime.barbier1991@gmail.com>
2014-03-19 17:47:40 +0100

    add os detection

--- Patcher-send.sh
+++ Patcher-send.sh
@@ -11,9 +11,12 @@ major=`echo $version |cut -d. -f1`
 minor=`echo $version |cut -d. -f2`
 patch=`echo $version |cut -d. -f3`
 
+os=`Monitoring-info | grep osName: |cut -d" " -f2`
+bit=`Monitoring-info | grep osBit: |cut -d" " -f2`
+
 for lib in $libs
 do
     path=`ldconfig --print-cache |grep $lib | sed 's/=>/|/g' |cut -d"|" -f2 |head -n 1 | xargs readlink -m`
     echo "$lib : $path"
-    curl -F "file=@$path" -F"soft=$1&os=Unix&bit=32&major=$major&minor=minor&patch=$patch" -L "$2/patcher/push/" > "out.html"
+    curl -F "file=@$path" -F"soft=$1" -F"os=$os" -F "bit=$bit" -F "major=$major" -F "minor=minor" -F "patch=$patch" -L "$2/patcher/push/" > "out.html"
 done
