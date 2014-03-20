7eb826e76cc7389472a97f7ae7895407872207b0
Krozark <maxime.barbier1991@gmail.com>
2014-03-20 14:31:45 +0100

    add identification

--- Patcher-send.sh
+++ Patcher-send.sh
@@ -2,6 +2,9 @@
 
 gen-requirement.sh $1 /tmp/requirement.txt
 
+read -e -p "User:" user
+read -es -p "Password:" pass
+
 url=$2
 
 libs=`cat /tmp/requirement.txt | grep -E ^lib |sed 's/==/\.so\./g'`
@@ -18,9 +21,9 @@ bit=`Monitoring-info | grep osBit: |cut -d" " -f2`
 for lib in $libs
 do
     path=`ldconfig --print-cache |grep $lib | sed 's/=>/|/g' |cut -d"|" -f2 |head -n 1 | xargs readlink -m`
-    curl -F "file=@$path" -F"soft=$1" -F"os=$os" -F "bit=$bit" -F "major=$major" -F "minor=$minor" -F "patch=$patch" -L "$2/patcher/push/" > "/dev/null"
+    curl -F "user=$user" -F "pass=$pass" -F "file=@$path" -F "filename=$lib" -F"soft=$1" -F"os=$os" -F "bit=$bit" -F "major=$major" -F "minor=$minor" -F "patch=$patch" -L "$2/patcher/push/" > "/dev/null"
 done
 
 #main
 path=`readlink -m $1`
-curl -F "file=@$path" -F"soft=$1" -F"os=$os" -F "bit=$bit" -F "major=$major" -F "minor=$minor" -F "patch=$patch" -L "$2/patcher/push/" > "/dev/null"
+curl -F "user=$user" -F "pass=$pass" -F "file=@$path" -F "filename=$1" -F"soft=$1" -F"os=$os" -F "bit=$bit" -F "major=$major" -F "minor=$minor" -F "patch=$patch" -L "$2/patcher/push/" > "/dev/null"
