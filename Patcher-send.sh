bd75b0e2b1c733f544c421705b71ae2d4a9a281a
Krozark <maxime.barbier1991@gmail.com>
2014-03-20 15:04:16 +0100

    maj

--- Patcher-send.sh
+++ Patcher-send.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+out="out.txt"
 gen-requirement.sh $1 /tmp/requirement.txt
 
 read -e -p "User:" user
 read -es -p "Password:" pass
+
+echo "Connect to $2 using user: $user" > $out
 
 url=$2
 
@@ -21,9 +24,14 @@ bit=`Monitoring-info | grep osBit: |cut -d" " -f2`
 for lib in $libs
 do
     path=`ldconfig --print-cache |grep $lib | sed 's/=>/|/g' |cut -d"|" -f2 |head -n 1 | xargs readlink -m`
-    curl -F "user=$user" -F "pass=$pass" -F "file=@$path" -F "filename=$lib" -F"soft=$1" -F"os=$os" -F "bit=$bit" -F "major=$major" -F "minor=$minor" -F "patch=$patch" -L "$2/patcher/push/" > "/dev/null"
+    echo "== send file $lib (path=$path) ==" >> $out
+    echo "response : ">> $out
+    curl -F "user=$user" -F "pass=$pass" -F "file=@$path" -F "filename=$lib" -F"soft=$1" -F"os=$os" -F "bit=$bit" -F "major=$major" -F "minor=$minor" -F "patch=$patch" -L "$2/patcher/push/" >> $out
+    echo "">> $out
 done
 
 #main
 path=`readlink -m $1`
-curl -F "user=$user" -F "pass=$pass" -F "file=@$path" -F "filename=$1" -F"soft=$1" -F"os=$os" -F "bit=$bit" -F "major=$major" -F "minor=$minor" -F "patch=$patch" -L "$2/patcher/push/" > "/dev/null"
+echo "== send exe file $1 (path=$path) ==" >> $out
+echo "response : ">> $out
+curl -F "user=$user" -F "pass=$pass" -F "main=true" -F "file=@$path" -F "filename=$1" -F"soft=$1" -F"os=$os" -F "bit=$bit" -F "major=$major" -F "minor=$minor" -F "patch=$patch" -L "$2/patcher/push/" >> $out
